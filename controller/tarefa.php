577cee077fbec4b4295c6877fbaa332f4cb1c9fd
Vitoria Poncio <vitoria.poncio@gmail.com>
2023-02-10 18:36:42 -0300

    initial commit

---
+++ controller/tarefa.php
@@ -0,0 +1,25 @@
+<?php
+
+class tarefa{
+
+    public $id;
+    public $titulo;
+    public $dever;
+
+    public function cadastrar(){
+
+    }
+
+    public function atualizar(){
+
+    }
+
+    public function excluir(){
+
+    }
+
+    public function mostrar(){
+        
+    }
+} 
+?>
